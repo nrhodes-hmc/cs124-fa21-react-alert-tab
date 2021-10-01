ddb37e76d18cc783c8f889ccff6ac4a26d199f88
Neil Rhodes <neil@pobox.com>
2021-10-01 12:01:11 -0700

    Refactor to separate Alert component

--- src/AlertApp.js
+++ src/AlertApp.js
@@ -1,11 +1,35 @@
 import './AlertApp.css';
 import {useState} from 'react'
 
+function Alert(props) {
+    return <div className={"backdrop"}>
+        <div className="modal">
+            {props.children}
+            <div className="alert-buttons">
+                <button className={"alert-button alert-cancel"} type={"button"}
+                        onClick={props.onClose}>
+                    Cancel
+                </button>
+                <button className={"alert-button alert-ok"} type={"button"}
+                        onClick={() => {
+                            props.onClose();
+                            props.onOK();
+                        }}>
+                    OK
+                </button>
+            </div>
+        </div>
+    </div>
+}
 function AlertApp() {
     const [showAlert, setShowAlert] = useState(true);
 
     function handleAlertOK() {
         console.log('the frob should be blitzened here');
+    }
+
+    function toggleModal() {
+        setShowAlert(!showAlert);
     }
 
     return (
@@ -16,28 +40,15 @@ function AlertApp() {
                 <button type={"button"} onClick={() => console.log('clicked')}>
                     A button
                 </button>
-                <button type={"button"} onClick={()=>setShowAlert(true)}>
+                <button type={"button"} onClick={toggleModal}>
                     Frob
                 </button>
             </div>
-            {showAlert && <div className={"backdrop"}>
-                <div className="modal">
+            {showAlert && <Alert onClose={toggleModal} onOK={handleAlertOK}>
+                <div>
                     Are you sure you want to Frob the blitzen?
-                    <div className="alert-buttons">
-                        <button className={"alert-button alert-cancel"} type={"button"}
-                                onClick={() => setShowAlert(false)}>
-                            Cancel
-                        </button>
-                        <button className={"alert-button alert-ok"} type={"button"}
-                                onClick={() => {
-                                    handleAlertOK();
-                                    setShowAlert(false)
-                                }}>
-                            OK
-                        </button>
-                    </div>
                 </div>
-            </div>}
+            </Alert>}
         </div>
     );
 }
