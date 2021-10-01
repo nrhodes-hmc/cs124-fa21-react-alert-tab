e81226ba574f6dc0f00a97c6d60704d67cdd930f
Neil Rhodes <neil@pobox.com>
2021-10-01 09:07:32 -0700

    Starter code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {useState} from 'react'
+
+function Tab(props) {
+    const classNames = ["tab-list-item"];
+    if (props.activeTab === props.label) {
+        classNames.push("tab-list-active");
+    }
+    return <li className={classNames.join(" ")}
+               onClick={() => props.onClickTab(props.label)}>
+        {props.label}
+    </li>
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [activeTab, setActiveTab] = useState("Tab 1");
+    return (
+        <div className="tabs">
+            <ol className="tab-list">
+                <Tab key="Tab 1"
+                     label={"Tab 1"}
+                     activeTab={activeTab}
+                     onClickTab={(label) => setActiveTab(label)}/>
+                <Tab key="Tab 2"
+                     label={"Tab 2"}
+                     activeTab={activeTab}
+                     onClickTab={(label) => setActiveTab(label)}/>
+            </ol>
+            {activeTab === "Tab 1" && <div>Hello, world!</div>}
+            {activeTab === "Tab 2" && <div>Goodbye, universe!</div>}
+        </div>
+    );
 }
 
 export default App;
